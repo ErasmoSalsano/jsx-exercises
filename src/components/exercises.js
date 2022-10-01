bbee4c654954cb33b46ed7445c8d7f71bee98590
ErasmoSalsano <erasmosalsano@gmail.com>
2022-10-01 17:07:54 +0200

    4

--- src/components/exercises.js
+++ src/components/exercises.js
@@ -8,3 +8,7 @@ export function HelloName(name){
 
 // If the name is not passed it will only show the rest of the content 
 // (in this case 'Hello, ')
+
+// JSX-04
+
+// The result of the expression would be rendered
